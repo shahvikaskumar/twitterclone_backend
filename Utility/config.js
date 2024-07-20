4c56efaa82aa373905c44019a2704019a008d3bd
vikas2991 <vikasshah508@gmail.com>
2024-07-20 21:50:54 +0530

    netlify add

--- Utility/config.js
+++ Utility/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 module.exports = {
-    MONGODB_URL:"mongodb://localhost:27017/twitter",
-    JWT_SECRET:"Tera@13_Bhikshu"
+    MONGODB_URL: process.env.MONGODB_URL,
+    JWT_SECRET: process.env.JWT_SECRET
 }
---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const conn = require('../Utility/connectdb');
+const serverless = require('serverless-http');
+const authroutes = require('../routes/auth_route');
+const userroutes = require('../routes/user_route');
+const tweetroutes = require('../routes/tweet_route');
+
+
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/.netlify/functions/index', authroutes);
+app.use('/.netlify/functions/index', tweetroutes);
+app.use('/.netlify/functions/index', userroutes);
+
+const startserver = async () => {
+    try {
+        await conn()
+        app.listen(port, () => {
+            console.log(`Server is running on a ${port}`);
+        });
+    }
+    catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+startserver();
+
+module.exports.handler = serverless(app);
